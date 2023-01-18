be15b28693f9733a31579e307216420419c497be
zubiii <zubair.javed.ch@gmail.com>
2023-01-17 21:22:23 -0600

    frtch api and added dearch bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import { Component } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState(
+        () => {
+          return { monsters: users }
+        },
+        () => {
+          console.log("Monsters => ", this.state.monsters)
+        }
+      ))
+  }
+
+  onSearchChnage = (event) => {
+    this.setState(() => {
+      return { searchField: event.target.value.toLowerCase() }
+    })
+  }
+
+  render() {
+
+    const filterMonsters = this.state.monsters.filter((monster) =>  {
+      return monster.name.toLowerCase().includes(this.state.searchField)
+    })
+
+    return (
+      <div className="App">
+        <input type="search" placeholder='Monster Search' onChange={this.onSearchChnage} />
+        {
+          filterMonsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
